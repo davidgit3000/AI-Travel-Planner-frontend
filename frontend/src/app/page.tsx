527dbd9dfccfb4a9474b9bff7576d89a7535ef60
David Lam <davidkarik5@gmail.com>
2025-03-19 20:17:05 -0700

    added dashboard page and not-found page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 /**
  * The homepage of the AI Travel Planner app.
@@ -10,8 +11,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-xl font-bold">AI Travel Planner</h1>
-        <p className="text-lg">Plan your next adventure with AI-powered travel recommendations.</p>  
+        <h1 className="text-xl font-bold">AI Travel Planner - Landing Page</h1>
+        <p className="text-lg">
+          Plan your next adventure with AI-powered travel recommendations.
+        </p>
+        <Button className="bg-blue-600 dark:hover:bg-blue-500 dark:hover:text-slate-100 text-white text-xs sm:text-lg">
+          <Link href="/dashboard">Start Planning</Link>
+        </Button>
       </main>
     </div>
   );
